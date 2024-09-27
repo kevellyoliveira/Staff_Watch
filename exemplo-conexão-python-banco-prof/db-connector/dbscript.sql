4740e3656270c738f365bf31edbb4e263b2fac9d
izabelleFigueiredo <izabelle.figueiredo@sptech.school>
2024-09-27 11:05:37 -0300

    organizando arquivos e adicionando web-data-viz

--- exemplo-conexão-python-banco-prof/db-connector/dbscript.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE logbateria;
-
-CREATE TABLE log(
-    id INT PRIMARY KEY auto_increment,
-    data_hora DATETIME,
-    alerta VARCHAR(255),
-    status_bateria DECIMAL(5,2)
-)CHARACTER SET='utf8mb4'; 
