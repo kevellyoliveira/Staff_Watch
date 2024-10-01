e67a1be56eca7c0542f74219d6c7617653fc0b5b
izabelleFigueiredo <izabelle.figueiredo@sptech.school>
2024-10-01 13:03:54 -0300

    alteracoes bd

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -6,8 +6,9 @@ var database = require("../database/config");
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var token = req.body.tokenServer;
 
-    usuarioModel.autenticar(email, senha)
+    usuarioModel.autenticar(email, senha, token)
         .then(
             function (resultadoAutenticar) {
                 console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
@@ -39,27 +40,27 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nomeEmp = req.body.nomeEmpServer;
     var cnpj = req.body.cnpjServer;
-    var token = req.body.tokenServer;
+    var nomeRep = req.body.nomeRepServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (nomeEmp == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (nickname == undefined) {
-        res.status(400).send("Seu nickname está undefined!");
-    }else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("Seu cnpj está undefined!");
+    }else if (nomeRep == undefined) {
+        res.status(400).send("Seu nomeRep está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Sua email está undefined!");
     } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (personagem == undefined) {
-        res.status(400).send("Seu personagem está undefined!");
+        res.status(400).send("Seu senha está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, cnpj, token, email, senha)
+        usuarioModel.cadastrar(nomeEmp, cnpj, nomeRep, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -11,17 +11,25 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, cnpj, token, email, senha) {
+function cadastrar(nomeEmp, cnpj, nomeRep, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO empresa (nome, cnpj, token, email, senha) VALUES 
-        ('${nome}', '${cnpj}', '${token}', '${email}', MD5('${senha}'));
+        INSERT INTO empresa (nomeEmp, cnpj, nomeRep, email, senhaRep) VALUES 
+        ('${nomeEmp}', '${cnpj}', '${nomeRep}', '${email}', MD5('${senha}'));
     `;
+
+    var instrucaoSql1 = `
+        SELECT idEmpresa FROM Empresa WHERE nomeEmp = ${nomeEmp};
+    `;
+    var instrucaoSql2 = `
+        INSERT INTO funcionario (nome, email, senha, fkEmpresa) VALUES 
+        ('${nomeRep}', '${email}', MD5('${senha}', ${instrucaoSql1})); `;
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    return database.executar(instrucaoSql, instrucaoSql1, instrucaoSql2);
 }
 
 module.exports = {
