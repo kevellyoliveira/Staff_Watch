255638786cadd624de46e3f4bccf6d9014bd68c5
Pedro01pinho <pedro.antunes@sptech.school>
2024-10-10 14:08:03 -0300

    Adicionando for no script python

--- documentos/captura-de-dados-python/TestePython3 - mini menu.py
+++ documentos/captura-de-dados-python/TestePython3 - mini menu.py
@@ -1,8 +1,10 @@
 import psutil
 import time
 import mysql.connector
-# import os
-# from dotenv import load_dotenv
+import tkinter as tk
+
+#import os
+#import dotenv
 
 config = {
     'user': 'root',
@@ -10,6 +12,9 @@ config = {
     'host': 'localhost',
     'database': 'StaffWatch'
 }
+root = tk.Tk()
+def sair():
+    root.destroy()
 
 # Estabelecendo a conexão
 try:
@@ -28,97 +33,101 @@ def menu():
     opcao = input("Escolha uma opção: ")
     if opcao == "1":
         menu_python()
+    elif opcao == "2":
+        sair()
+        
         
 
 def menu_python():
             idComputador = input("Digite o id do computador que deseja monitorar: ")
             cursor = mydb.cursor()
-            mem = psutil.virtual_memory()
-            print(f"\nUso de RAM: {mem.percent}% ({mem.used / (1024 ** 3):.2f} GB usado de {mem.total / (1024 ** 3):.2f} GB total)")
-            memUso = mem.used / (1024 ** 3)
-            memTotal = mem.total / (1024 ** 3)
-            memPerc = mem.percent
+            for i in range(15):
+                mem = psutil.virtual_memory()
+                print(f"\nUso de RAM: {mem.percent}% ({mem.used / (1024 ** 3):.2f} GB usado de {mem.total / (1024 ** 3):.2f} GB total)")
+                memUso = mem.used / (1024 ** 3)
+                memTotal = mem.total / (1024 ** 3)
+                memPerc = mem.percent
 
-            add_mem = ("""INSERT INTO captura
-                        (idCaptura, captura,fkAuxComponente, fkComponente, fkComputador)
-                        VALUES (default,%s,6,2,%s),
-                        (default,%s,7,2,%s),
-                        (default,%s,8,2,%s)""")
+                add_mem = ("""INSERT INTO captura
+                            (idCaptura, captura,fkAuxComponente, fkComponente, fkComputador)
+                            VALUES (default,%s,6,2,%s),
+                            (default,%s,7,2,%s),
+                            (default,%s,8,2,%s)""")
 
-            data_mem = [memUso, idComputador, memTotal, idComputador, memPerc, idComputador]
+                data_mem = [memUso, idComputador, memTotal, idComputador, memPerc, idComputador]
 
-            cursor.execute(add_mem, data_mem)
-            mydb.commit()
-            print(cursor.rowcount, "registro inserido - memória")
+                cursor.execute(add_mem, data_mem)
+                mydb.commit()
+                print(cursor.rowcount, "registro inserido - memória")
 
-            # Obtém e exibe o uso de disco
-            disk = psutil.disk_usage('/')
-            print(f"Uso de Disco: {disk.percent}% ({disk.used / (1024 ** 3):.2f} GB usado de {disk.total / (1024 ** 3):.2f} GB total)")
-            discoUso = disk.used / (1024 ** 3)
-            discoTotal = disk.total / (1024 ** 3)
-            discoPerc = disk.percent
+                # Obtém e exibe o uso de disco
+                disk = psutil.disk_usage('/')
+                print(f"Uso de Disco: {disk.percent}% ({disk.used / (1024 ** 3):.2f} GB usado de {disk.total / (1024 ** 3):.2f} GB total)")
+                discoUso = disk.used / (1024 ** 3)
+                discoTotal = disk.total / (1024 ** 3)
+                discoPerc = disk.percent
 
-            add_disco = ("""INSERT INTO captura
-                        (idCaptura, captura,fkAuxComponente, fkComponente, fkComputador)
-                        VALUES (default,%s,9,3,%s),
-                        (default,%s,10,3,%s),
-                        (default,%s,11,3,%s)""")
+                add_disco = ("""INSERT INTO captura
+                            (idCaptura, captura,fkAuxComponente, fkComponente, fkComputador)
+                            VALUES (default,%s,9,3,%s),
+                            (default,%s,10,3,%s),
+                            (default,%s,11,3,%s)""")
 
-            data_disco = [discoUso,idComputador, discoTotal, idComputador, discoPerc,idComputador]
+                data_disco = [discoUso,idComputador, discoTotal, idComputador, discoPerc,idComputador]
 
-            cursor.execute(add_disco, data_disco)
-            mydb.commit()
-            print(cursor.rowcount, "registro inserido - disco")
-            # Obtém e exibe o uso da CPU
-            cpu_percent = psutil.cpu_percent(interval=None)
+                cursor.execute(add_disco, data_disco)
+                mydb.commit()
+                print(cursor.rowcount, "registro inserido - disco")
+                # Obtém e exibe o uso da CPU
+                cpu_percent = psutil.cpu_percent(interval=None)
 
-            add_cpu = ("""INSERT INTO captura
-                        (idRegistro, Registro, fkAuxComponente,fkComponente, fkComputador)
-                        VALUES (default,%s,12,4,%s)""")
+                add_cpu = ("""INSERT INTO captura
+                            (idRegistro, Registro, fkAuxComponente,fkComponente, fkComputador)
+                            VALUES (default,%s,12,4,%s)""")
 
-            data_cpu = [cpu_percent, idComputador]
+                data_cpu = [cpu_percent, idComputador]
 
-            cursor.execute(add_disco, data_disco)
-            mydb.commit()
-            print(cursor.rowcount, "registro inserido - cpu")
+                cursor.execute(add_disco, data_disco)
+                mydb.commit()
+                print(cursor.rowcount, "registro inserido - cpu")
 
-            print(f"Uso Total da CPU: {cpu_percent}%")
-            cpuPerc = cpu_percent
-            # Obtém e exibe o uso da CPU por núcleo
-            cpu_per_core = psutil.cpu_percent(percpu=True, interval=None)
-            print("Uso da CPU por Núcleo:")
-            for i, perc in enumerate(cpu_per_core):
-                print(f"  Núcleo {i}: {perc}%")
+                print(f"Uso Total da CPU: {cpu_percent}%")
+                cpuPerc = cpu_percent
+                # Obtém e exibe o uso da CPU por núcleo
+                cpu_per_core = psutil.cpu_percent(percpu=True, interval=None)
+                print("Uso da CPU por Núcleo:")
+                for i, perc in enumerate(cpu_per_core):
+                    print(f"  Núcleo {i}: {perc}%")
 
-                # Obtém e exibe o uso da rede
-            net_io = psutil.net_io_counters()
-            print(f"\nUso de Rede:")
-            print(f"  Bytes recebidos: {net_io.bytes_recv / (1024 ** 1):.2f} KB")
-            bytesReceb = net_io.bytes_recv / (1024 ** 2)
-            print(f"  Bytes enviados: {net_io.bytes_sent / (1024 ** 1):.2f} KB")
-            bytesEnv = net_io.bytes_sent / (1024 ** 2)
-            print(f"  Pacotes recebidos: {net_io.packets_recv}")
-            pctReceb = net_io.packets_recv
-            print(f"  Pacotes enviados: {net_io.packets_sent}")
-            pctEnv = net_io.packets_sent
-            print(f"\n{'PID':<10} {'Nome':<30} {'Uso de CPU (%)':<15}")
-            print("="*60)
-
-
-            add_rede = ("""INSERT INTO captura
-                        (idCaptura, Captura, fkAuxComponente,fkComponente, fkComputador)
-                        VALUES (default,%s,2,1,%s),
-                        (default,%s,3,1,%s),
-                        (default,%s,4,1,%s),
-                        (default,%s,5,1,%s)""")
-
-            data_rede = [bytesEnv,idComputador, bytesReceb,idComputador, pctReceb,idComputador, pctEnv, idComputador]
-            print(type(data_rede))
+                    # Obtém e exibe o uso da rede
+                net_io = psutil.net_io_counters()
+                print(f"\nUso de Rede:")
+                print(f"  Bytes recebidos: {net_io.bytes_recv / (1024 ** 1):.2f} KB")
+                bytesReceb = net_io.bytes_recv / (1024 ** 2)
+                print(f"  Bytes enviados: {net_io.bytes_sent / (1024 ** 1):.2f} KB")
+                bytesEnv = net_io.bytes_sent / (1024 ** 2)
+                print(f"  Pacotes recebidos: {net_io.packets_recv}")
+                pctReceb = net_io.packets_recv
+                print(f"  Pacotes enviados: {net_io.packets_sent}")
+                pctEnv = net_io.packets_sent
+                print(f"\n{'PID':<10} {'Nome':<30} {'Uso de CPU (%)':<15}")
+                print("="*60)
 
 
-            cursor.execute(add_rede, data_rede)
-            mydb.commit()
-            print(cursor.rowcount, "registro inserido")
+                add_rede = ("""INSERT INTO captura
+                            (idCaptura, Captura, fkAuxComponente,fkComponente, fkComputador)
+                            VALUES (default,%s,2,1,%s),
+                            (default,%s,3,1,%s),
+                            (default,%s,4,1,%s),
+                            (default,%s,5,1,%s)""")
+
+                data_rede = [bytesEnv,idComputador, bytesReceb,idComputador, pctReceb,idComputador, pctEnv, idComputador]
+                print(type(data_rede))
+
+
+                cursor.execute(add_rede, data_rede)
+                mydb.commit()
+                print(cursor.rowcount, "registro inserido")
             monitor_system()
             
 def print_system_info():
@@ -229,7 +238,7 @@ def print_system_info():
 
 def monitor_system(interval=5):
     while True:
-        print_system_info()
+        menu()
         time.sleep(interval)
     #if conn.is_connected():
      #   conn.close()
