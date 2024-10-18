903b61774d25b9431ed37f9977dd4f89df0e52a9
MatheusFermoselle <matheus.vsilva@sptech.school>
2024-10-18 17:16:08 -0300

    Terminando a ETL. Foi feito o load para o banco de dados, com os dados dos gráfico, que foram tratados

--- documentos/ETL - StaffWatch (Teste).R
+++ documentos/ETL - StaffWatch (Teste).R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 
 # Conectar ao banco de dados
-con <- dbConnect(MySQL(), user='root', password='Crfsruan1', dbname='StaffWatch', host='localhost')
+con <- dbConnect(MySQL(), user='root', password='sua_senha', dbname='StaffWatch', host='localhost')
 
 # Consultar os dados
 query <- "SELECT ca.dataCaptura, 
@@ -18,7 +18,7 @@ ORDER BY ca.fkComputador, ca.dataCaptura;
 "
 
 data_raw <- dbGetQuery(con, query)
-
+View(data_raw)
 
 # Verificando os dados extraídos
 print(head(data_raw))
@@ -38,6 +38,22 @@ ggplot(data_processed, aes(x = dataCaptura, y = valor_megabytes, color = as.fact
        color = "Computador") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Preparar os dados para inserção no banco de dados
+data_to_db <- data_processed %>%
+  select(dataCaptura, valor_megabytes, fkComputador) %>%
+  rename(data_captura = dataCaptura,
+         dados_mb = valor_megabytes,
+         computador = fkComputador)
+
+# Inserir os dados linha por linha no banco de dados
+for (i in 1:nrow(data_to_db)) {
+  query_insert <- sprintf("INSERT INTO grafico_trafego_rede (data_captura, dados_mb, computador) VALUES ('%s', %f, %d)",
+                          data_to_db$data_captura[i],
+                          data_to_db$dados_mb[i],
+                          data_to_db$computador[i])
+  dbSendQuery(con, query_insert)
+}
 
 # Fechar a conexão
 dbDisconnect(con)
