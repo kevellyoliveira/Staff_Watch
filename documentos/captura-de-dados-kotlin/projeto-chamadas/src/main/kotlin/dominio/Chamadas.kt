19abb3fd04a0cd2fe3f010dca8c090a22d04c434
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-11-18 18:16:29 -0300

    Subindo alterações na minha dashboard

--- documentos/captura-de-dados-kotlin/projeto-chamadas/src/main/kotlin/dominio/Chamadas.kt
+++ documentos/captura-de-dados-kotlin/projeto-chamadas/src/main/kotlin/dominio/Chamadas.kt
@@ -2,14 +2,14 @@ package dominio
 
 class Chamadas {
 
-    var chamadaRecebida: Int = 0;
-    var chamadaAtendida: Int = 0;
-    var chamadaPerdida: Int = 0;
-    var tempoChamada: Int = 0;
-    var tempoEspera: Int = 0;
-    var eficienciaChamada: Double = 0.0;
-    var fkFuncionario: Int = 0;
-    var fkEmpresa: Int = 0;
+    var chamadaRecebida: Int? = 0;
+    var chamadaAtendida: Int? = 0;
+    var chamadaPerdida: Int? = 0;
+    var tempoChamada: Int? = 0;
+    var tempoEspera: Int? = 0;
+    var eficienciaChamada: Double? = 0.0;
+    var fkFuncionario: Int? = 0;
+    var fkEmpresa: Int? = 0;
 
 
 }
--- documentos/captura-de-dados-kotlin/projeto-chamadas/src/main/kotlin/repositorio/ChamadaRepository.kt
+++ documentos/captura-de-dados-kotlin/projeto-chamadas/src/main/kotlin/repositorio/ChamadaRepository.kt
@@ -48,70 +48,70 @@ class ChamadaRepository {
         return qtdLinhasAfetadas > 0
     }
 
-    fun processarEvento(event: ManagerEvent) {
-
-        var listaFuncionario = listarFuncionarios();
-
-        when (event) {
-            is NewStateEvent -> {
-                println("Chamada em novo estado: ${event.state} - Número: ${event.callerIdNum}")
-
-                // Verifica se a chamada está tocando (Ringing)
-                if (event.state == "Ringing") {
-                    // Marca o início do tempo de espera
-                    val tempoInicioEspera = System.currentTimeMillis()
-
-                    // Cria um objeto de chamada com valores iniciais
-                    val chamada = Chamadas().apply {
-                        chamadaRecebida = 1 // A chamada foi recebida
-                        chamadaAtendida = 0
-                        chamadaPerdida = 0
-                        tempoChamada = 0
-                        tempoEspera = 0 // Inicializando o tempo de espera
-                        eficienciaChamada = 0.0// Inicializando a eficiência
-                        fkFuncionario = 1 // Atribuindo funcionario dinamicamente
-                        fkEmpresa = 1 // Atribuindo empresa dinamicamente
-                    }
-
-                    // Salva a chamada na base de dados
-                  salvarChamada(chamada)
-                }
-            }
-
-            is AnswerEvent -> {
-                // Quando a chamada for atendida
-                println("Chamada atendida: ${event.callerIdNum}")
-
-                // Atualiza a chamada para indicar que foi atendida
-                val chamada = listaFuncionario[1].telefone(event.callerIdNum) // Recupera a chamada
-                chamada?.let {
-                    it.chamadaAtendida = 1 // Marca como atendida
-
-                    // Calcula o tempo total de chamada
-                    val tempoFinal = System.currentTimeMillis()
-                    it.tempoChamada = (tempoFinal - it.tempoInicioEspera) / 1000 // Tempo de chamada em segundos
-                    it.eficienciaChamada = if (it.tempoChamada > 0) it.tempoEspera.toDouble() / it.tempoChamada else 0.0 // Calcula a eficiência da chamada
-
-                    chamadaRepository.salvarChamada(it) // Atualiza os dados da chamada
-                }
-            }
-
-            is HangupEvent -> {
-                // Quando a chamada é desligada
-                println("Chamada desligada: ${event.causeTxt}")
-
-                // Atualiza a chamada com o status de desligada
-                val chamada = chamadaRepository.obterChamadaPorNumero(event.callerIdNum) // Recupera a chamada
-                chamada?.let {
-                    it.chamadaPerdida = 1 // Marca a chamada como perdida se não foi atendida
-                    val tempoFinal = System.currentTimeMillis()
-                    it.tempoChamada = (tempoFinal - it.tempoInicioEspera) / 1000 // Tempo de chamada em segundos
-                    it.eficienciaChamada = if (it.tempoChamada > 0) it.tempoEspera.toDouble() / it.tempoChamada else 0.0 // Atualiza a eficiência
-
-                    chamadaRepository.salvarChamada(it) // Atualiza a chamada na base
-                }
-            }
-        }
-    }
+//    fun processarEvento(event: ManagerEvent) {
+//
+//        var listaFuncionario = listarFuncionarios();
+//
+//        when (event) {
+//            is NewStateEvent -> {
+//                println("Chamada em novo estado: ${event.state} - Número: ${event.callerIdNum}")
+//
+//                // Verifica se a chamada está tocando (Ringing)
+//                if (event.state == "Ringing") {
+//                    // Marca o início do tempo de espera
+//                    val tempoInicioEspera = System.currentTimeMillis()
+//
+//                    // Cria um objeto de chamada com valores iniciais
+//                    val chamada = Chamadas().apply {
+//                        chamadaRecebida = 1 // A chamada foi recebida
+//                        chamadaAtendida = 0
+//                        chamadaPerdida = 0
+//                        tempoChamada = 0
+//                        tempoEspera = 0 // Inicializando o tempo de espera
+//                        eficienciaChamada = 0.0// Inicializando a eficiência
+//                        fkFuncionario = 1 // Atribuindo funcionario dinamicamente
+//                        fkEmpresa = 1 // Atribuindo empresa dinamicamente
+//                    }
+//
+//                    // Salva a chamada na base de dados
+//                  salvarChamada(chamada)
+//                }
+//            }
+//
+//            is AnswerEvent -> {
+//                // Quando a chamada for atendida
+//                println("Chamada atendida: ${event.callerIdNum}")
+//
+//                // Atualiza a chamada para indicar que foi atendida
+//                val chamada = listaFuncionario[1].telefone(event.callerIdNum) // Recupera a chamada
+//                chamada?.let {
+//                    it.chamadaAtendida = 1 // Marca como atendida
+//
+//                    // Calcula o tempo total de chamada
+//                    val tempoFinal = System.currentTimeMillis()
+//                    it.tempoChamada = (tempoFinal - it.tempoInicioEspera) / 1000 // Tempo de chamada em segundos
+//                    it.eficienciaChamada = if (it.tempoChamada > 0) it.tempoEspera.toDouble() / it.tempoChamada else 0.0 // Calcula a eficiência da chamada
+//
+//                    chamadaRepository.salvarChamada(it) // Atualiza os dados da chamada
+//                }
+//            }
+//
+//            is HangupEvent -> {
+//                // Quando a chamada é desligada
+//                println("Chamada desligada: ${event.causeTxt}")
+//
+//                // Atualiza a chamada com o status de desligada
+//                val chamada = chamadaRepository.obterChamadaPorNumero(event.callerIdNum) // Recupera a chamada
+//                chamada?.let {
+//                    it.chamadaPerdida = 1 // Marca a chamada como perdida se não foi atendida
+//                    val tempoFinal = System.currentTimeMillis()
+//                    it.tempoChamada = (tempoFinal - it.tempoInicioEspera) / 1000 // Tempo de chamada em segundos
+//                    it.eficienciaChamada = if (it.tempoChamada > 0) it.tempoEspera.toDouble() / it.tempoChamada else 0.0 // Atualiza a eficiência
+//
+//                    chamadaRepository.salvarChamada(it) // Atualiza a chamada na base
+//                }
+//            }
+//        }
+//    }
 
 }
