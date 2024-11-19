05611dc6d25d77b9b87d652741ad3fdb0cc4ef70
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-11-19 14:38:57 -0300

    Subindo atualizações na dashboard

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const openModal = document.querySelectorAll(".open-modal");
-    const closeModal = document.querySelector("#close-modal");
-    const modal = document.querySelector("#modal");
-    const fade = document.querySelector("#fade");
+// Seleção de elementos
+const openModal = document.querySelectorAll(".open-modal");
+const closeModal = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
 
-    const openModal2 = document.querySelectorAll(".open-modal2");
-    const closeModal2 = document.querySelector("#close-modal2");
-    const modal2 = document.querySelector("#modal2");
-    const fade2 = document.querySelector("#fade2");
-    const botaoModalEdit = document.getElementById("editarBot");
+// Função para alternar a visibilidade do modal
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
 
-    const toggleModal = () => {
-        modal.classList.toggle("hide");
-        fade.classList.toggle("hide");
-    };
-
-    const toggleModal2 = () => {
-        modal2.classList.toggle("hide");
-        fade2.classList.toggle("hide");
-    };
-
-    // Adiciona evento para abrir o primeiro modal ao clicar no card
-    openModal.forEach(button => {
-        button.addEventListener("click", (event) => {
-            if (event.target !== botaoModalEdit) {
-                toggleModal();
-            } else {
-                toggleModal2();
-            }
-        });
-    });
-
-    // Adiciona eventos para fechar os modais
-    closeModal.addEventListener("click", toggleModal);
-    fade.addEventListener("click", toggleModal);
-
-    closeModal2.addEventListener("click", toggleModal2);
-    fade2.addEventListener("click", toggleModal2);
-
-    // Adiciona evento para o botão de edição
-    botaoModalEdit.addEventListener("click", (event) => {
-        event.stopPropagation();
-        toggleModal2();
-    });
+// Adiciona evento de clique para abrir o modal
+openModal.forEach(button => {
+    button.addEventListener("click", toggleModal);
 });
 
-
+// Adiciona evento de clique para fechar o modal
+fade.addEventListener("click", toggleModal);
+closeModal.addEventListener("click", toggleModal);
